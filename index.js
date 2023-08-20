1747201551b2faa5987fe57b9ce6b977851d4915
Rajesh karwasara <140477625+Rajeshkarwasara@users.noreply.github.com>
2023-08-20 10:06:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,82 @@
+let p = 5
+const p1 = 10
+p = 10
+
+let str = "hello"
+console.log(p + p1)
+console.log(typeof (p))
+
+
+
+// next
+let x = 2;
+let y = 2;
+x += 3
+x++;
+x--;
+let z = x % y;
+console.log(z);
+
+
+
+// next
+let age = 19;
+if (age === 18) { console.log("you can vot", age) }
+// if (age>18) { console.log("you can vot", age) }
+// else if (age > 70) { console.log("sorrt denn you are budha") }
+else { console.log("not") }
+
+
+
+// next
+let age1 =18;
+if(age1>=18 && age1<=50){
+    console.log("eligibel")
+}
+else{
+    console.log("not eligibel")
+}
+
+
+
+
+// next
+function sam(a,b){
+    let = a;
+    let = b;
+   let c=a+b;
+   console.log("+",c)
+   let = a;
+    let = b;
+   let d=a-b;
+   console.log('-',d)
+   
+}
+sam(2,2)
+
+
+
+
+// next
+function rajesh(){
+    let a=2;
+    let b=5;
+    let e=a+b;
+    return e;
+}
+let j = rajesh();
+console.log("return",j)
+
+
+
+// next
+function sam2(c,d){
+    let add =c+d;
+    let mutliply=c*d;
+    console.log("addition",add,"mutliply",mutliply,)
+} 
+sam2(12,2)
+
+
+
+
